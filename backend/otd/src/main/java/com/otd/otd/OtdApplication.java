12aaa0dff83aa667f442745708f9f254ddeceb3c
hi-inbeom <id4mail2g@gmail.com>
2025-02-25 20:49:18 +0900

    프로젝트 기본 설정정

---
+++ backend/otd/src/main/java/com/otd/otd/OtdApplication.java
@@ -0,0 +1,13 @@
+package com.otd.otd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtdApplication.class, args);
+	}
+
+}
---
+++ backend/otd/src/test/java/com/otd/otd/OtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otd.otd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
